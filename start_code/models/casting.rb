43569a393eb5b8cbf7fb8f9b5f74a20d9e66c776
Rumen Roger <rumenroger@gmail.com>
2020-05-28 15:54:42 +0100

    casting.rb save()

--- start_code/models/casting.rb
+++ start_code/models/casting.rb
@@ -5,4 +5,31 @@ require_relative("casting")
 
 class Casting
 
+  attr_reader :id
+  attr_accessor :star_id, :movie_id, :fee
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @star_id = options['star_id'].to_i
+    @movie_id = options['movie_id'].to_i
+    @fee = options['fee'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO castings
+    (
+      star_id,
+      movie_id,
+      fee
+    )
+    VALUES
+    (
+      $1, $2, $3
+    )
+    RETURNING id"
+    values = [@star_id, @movie_id, @fee]
+    casting = SqlRunner.run(sql, values).first
+    @id = casting['id'].to_i
+  end
+
 end
